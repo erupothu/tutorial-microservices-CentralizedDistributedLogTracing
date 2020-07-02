44764e09772b88909a4b3e7a8af11ee1ddc014b6
Erupothu Harish <erupothu@users.noreply.github.com>
2020-07-02 20:24:14 +0530

    Add files via upload
    book services

---
+++ book-service/src/main/java/com/tutorial/BookApp.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableScheduling
+@EnableFeignClients
+public class BookApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BookApp.class, args);
+	}
+
+}
